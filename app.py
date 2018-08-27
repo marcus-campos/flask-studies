a0f466851c3b0d8d881fe05aa1130ffcd6a45164
Marcus Vinícius Campos <marcus.campos@sympla.com.br>
2018-08-27 15:12:55 -0300

    Create stores endpoints

--- app.py
+++ app.py
@@ -1,9 +1,31 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-	return "Hello, world!"
+stores = [
+	{
+		'name': "My store",
+		'items': [
+			{
+				'name': 'My item',
+				'price': 10.59
+			}
+		]
+	}
+]
+
+
+@app.route('/store', methods=['POST'])
+def create_store():
+	pass
+
+@app.route('/store/<string:name>')
+def get_store(name):
+	pass
+
+@app.route('/store')
+def get_stores():
+	return jsonify({'stores': stores})
+	pass
 
 app.run(debug=True,host='0.0.0.0',port=80)
