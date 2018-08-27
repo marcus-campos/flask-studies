05b031b93a5f5bf28d511913726a21cfd00f1ac3
Marcus Vinícius Campos <marcus.campos@sympla.com.br>
2018-08-27 15:19:26 -0300

    Search into store, and get stores

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -17,11 +17,20 @@ stores = [
 
 @app.route('/store', methods=['POST'])
 def create_store():
-	pass
+	request_data = request.get_json()
+	new_store = {
+		'name': request_data['name'],
+		'items': []
+	}
+	stores.append(new_store)
+	return jsonify(new_store)
 
 @app.route('/store/<string:name>')
 def get_store(name):
-	pass
+	for store in stores:
+		if store['name'] == name:
+			return jsonify(store)
+	return jsonify({'message': 'store not found'})
 
 @app.route('/store')
 def get_stores():
