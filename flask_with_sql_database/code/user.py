92d1eb957874b217635a1d94b623ac67c713a439
Marcus Vinícius Campos <marcus.campos@sympla.com.br>
2018-09-03 11:18:17 -0300

    Validate duplicated usernames

--- flask_with_sql_database/code/user.py
+++ flask_with_sql_database/code/user.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask_restful import Resource, regparse
+from flask_restful import Resource, reqparse
 
 class User:
 	def __init__(self, _id, username, password):
@@ -55,6 +55,9 @@ class UserRegister(Resource):
 
 	def post(self):
 		data = UserRegister.parser.parse_args()
+
+		if User.find_by_username(data['username']):
+			return {"message": "A user with that username already exists"}, 400
 
 		connection = sqlite3.connect('data.db')
 		cursor = connection.cursor()
